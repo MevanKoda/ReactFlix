daeda99eca492fb44b756d8a0350b3f3b8dabde1
Mevan Kodagoda <136101449+MevanKoda@users.noreply.github.com>
2026-07-01 21:28:02 +0530

    Add mobile sidebar navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,24 @@
-
+import {useState,React} from 'react'
 import {NavLink,BrowserRouter} from 'react-router-dom'
 import { GiHamburgerMenu } from "react-icons/gi";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { IoSearch } from "react-icons/io5";
 
+import SideBar from '../components/Sidebar'
+
 
 function Header(){
+
+    const [isOpened, setIsOpened] = useState(false)
+
+
+
+
     return(
         <>
-            <div className="flex justify-between items-center w-full p-8 bg-gray-900">
+            <div className={`justify-between items-center w-full p-8 bg-gray-900 ${isOpened ? "hidden" : "flex"}`}>
                 <div className="flex items-center gap-6"> 
-                    <GiHamburgerMenu className='scale-140 text-blue-400 lg:hidden' />
+                    <button onClick={()=>setIsOpened(!isOpened)}><GiHamburgerMenu className='scale-140 text-blue-400 lg:hidden' /></button>
                     <h1 className="text-2xl font-semibold text-blue-400">ReactFlix</h1>
                     <ul className="hidden lg:flex lg:ml-4 lg:gap-8 lg:text-md text-gray-300">
                         <li><NavLink to="/ReactFlix/">Home</NavLink></li>
@@ -35,6 +43,9 @@ function Header(){
                     <FaRegCircleUser className="size-6 text-white"/>
                 </div>
             </div>
+
+
+        <SideBar isOpened={isOpened} setIsOpened={setIsOpened}></SideBar>
         
         </>
     )
